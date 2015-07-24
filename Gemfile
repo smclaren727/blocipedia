3d24e1f1e48a1f2def240470ba59b10ef34ef670
smclaren727 <smclaren727@gmail.com>
2015-07-24 14:23:47 -0400

    Gemfile updated

--- Gemfile
+++ Gemfile
@@ -16,9 +16,6 @@ group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
-end
-
-group :development do
   gem 'sqlite3'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
 end
