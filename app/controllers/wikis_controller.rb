8a9252150287870b62a3e3888f7d8f2435ce5048
smclaren727 <smclaren727@gmail.com>
2015-07-28 16:11:30 -0400

    Added edit and delete controllers and views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,7 +1,52 @@
 class WikisController < ApplicationController
 
   def index
+    @wikis = Wiki.all
+  end
 
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    if @wiki.save
+      flash[:notice] = "Wiki entry was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the entry. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki entry was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the entry. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+   @wiki = Wiki.find(params[:id])
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error deleting the entry."
+      render :show
+    end
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,16 @@ require 'faker'
 end
 users = User.all
 
+# Create Wikis
+20.times do
+  wiki = Wiki.create!(
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+    )
+end
+wikis = Wiki.all
+
 puts "Seed finished"
 puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
